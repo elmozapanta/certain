42c10dd0910791c5ae646700efe52d60684ca881
elmozapanta <elmozapanta@yahoo.com>
2025-03-31 21:24:00 +0000

    Fix build - fixing a bug

--- src/extensions/onboarding_dashlet/reducers.ts
+++ src/extensions/onboarding_dashlet/reducers.ts
@@ -6,7 +6,7 @@ import * as actions from './actions';
 /**
  * reducer for changes to interface settings
  */
-const settingsReducer: IReducerSpec = {
+let settingsReducer: IReducerSpec = {
   reducers: {
     [actions.completeStep as any]: (state, payload) => setSafe(state, ['steps', payload], true),
     [actions.resetSteps as any]: (state) => setSafe(state, ['steps'], {}),
