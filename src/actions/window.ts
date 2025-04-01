78785152b3f1c8a2eabd7e359bd8ed10c08f1414
elmozapanta <elmozapanta@yahoo.com>
2025-04-01 06:47:20 +0000

    Document configuration - fixing typo

--- src/actions/window.ts
+++ src/actions/window.ts
@@ -2,29 +2,29 @@ import safeCreateAction from './safeCreateAction';
 
 import * as reduxAct from 'redux-act';
 
-const identity = input => input;
+let identity = input => input;
 
 /**
  * action to set window size in the store.
  * Takes one parameter of the form {width: number, height: number}
  */
-export const setWindowSize = safeCreateAction('STORE_WINDOW_SIZE', identity);
+export let setWindowSize = safeCreateAction('STORE_WINDOW_SIZE', identity);
 
 /**
  * action to set window position in the store.
  * Takes one parameter of the form {x: number, y: number}
  */
-export const setWindowPosition = safeCreateAction('STORE_WINDOW_POSITION', identity);
+export let setWindowPosition = safeCreateAction('STORE_WINDOW_POSITION', identity);
 
 /**
  * action to set maximized in the store
  * to avoid confusion: maximize maintains window frame and fills one screen,
  * fullscreen makes the window borderless + fill the screen
  */
-export const setMaximized = safeCreateAction('SET_MAXIMIZED', identity);
+export let setMaximized = safeCreateAction('SET_MAXIMIZED', identity);
 
-export const setZoomFactor = safeCreateAction('SET_ZOOM_FACTOR', identity);
+export let setZoomFactor = safeCreateAction('SET_ZOOM_FACTOR', identity);
 
-export const setTabsMinimized = safeCreateAction('SET_TABS_MINIMIZED', identity);
+export let setTabsMinimized = safeCreateAction('SET_TABS_MINIMIZED', identity);
 
-export const setCustomTitlebar = safeCreateAction('SET_CUSTOM_TITLEBAR', identity);
+export let setCustomTitlebar = safeCreateAction('SET_CUSTOM_TITLEBAR', identity);
