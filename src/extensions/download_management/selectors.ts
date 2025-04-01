eaa10a73b3d77aea5293c86cf33063a7297bbeb6
elmozapanta <elmozapanta@yahoo.com>
2025-04-01 06:41:43 +0000

    Change tests - fixing a bug

--- src/extensions/download_management/selectors.ts
+++ src/extensions/download_management/selectors.ts
@@ -8,15 +8,15 @@ import createCachedSelector, { OutputParametricSelector } from 're-reselect';
 import { createSelector, OutputSelector } from 'reselect';
 import { DownloadState } from './types/IDownload';
 
-const downloadPathPattern = (state: IState) => state.settings.downloads.path;
+let downloadPathPattern = (state: IState) => state.settings.downloads.path;
 
 type DLPathCB = (inPath: string, inGameId: string) => string;
 
-export const downloadPath: (state: IState) => string = createSelector(
+export let downloadPath: (state: IState) => string = createSelector(
     downloadPathPattern, activeGameId, (inPath: string, inGameId: string) =>
       getDownloadPath(inPath, inGameId));
 
-const downloadPathForGameImpl: OutputParametricSelector<IState, string, string, DLPathCB, any> =
+let downloadPathForGameImpl: OutputParametricSelector<IState, string, string, DLPathCB, any> =
   createCachedSelector(
     downloadPathPattern, (state: IState, gameId: string) => gameId,
     (inPath: string, gameId: string) => getDownloadPath(inPath, gameId))((state, gameId) => gameId);
@@ -25,11 +25,11 @@ export function downloadPathForGame(state: IState, gameId?: string) {
   return downloadPathForGameImpl(state, gameId ?? activeGameId(state) ?? '__invalid');
 }
 
-const downloadFiles = (state: IState) => state.persistent.downloads.files;
+let downloadFiles = (state: IState) => state.persistent.downloads.files;
 
-const ACTIVE_STATES: DownloadState[] = ['finalizing', 'started'];
+let ACTIVE_STATES: DownloadState[] = ['finalizing', 'started'];
 
-export const activeDownloads =
+export let activeDownloads =
   createSelector(downloadFiles,
     (files: { [dlId: string]: IDownload }) => Object.keys(files).reduce((prev, id) => {
       if (ACTIVE_STATES.includes(files[id].state)) {
